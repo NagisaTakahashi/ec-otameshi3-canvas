a79d53cacee3fe98f8417e1dd59352e2e93fbd67
nagisa_takahashi@recouture-inc.com <nagisa_takahashi@outlook.jp>
2022-11-07 03:12:39 +0900

    フッター動き追加、Canvas生成、フッターメニューにJQ指定

---
+++ color.js
@@ -0,0 +1,22 @@
+// alert(1111);
+
+
+
+//以下、カラーを表示分けするための配列定義
+
+var color_list= [
+  {"colornum":"01","colorname":"NVY","color_code":"3A3A49","RGB":"58,58,73",},
+  {"colornum":"02","colorname":"KKI","color_code":"5D6402","RGB":"93,100,2",},
+  {"colornum":"03","colorname":"LIME","color_code":"CFE375","RGB":"207,207,117",},
+  {"colornum":"04","colorname":"PPL","color_code":"C20F7C","RGB":"194,15,124",},
+  {"colornum":"05","colorname":"RED","color_code":"F32359","RGB":"243,35,89",},
+  {"colornum":"06","colorname":"MNT","color_code":"CBF1EF","RGB":"203,241,239",},
+];
+
+
+
+for( let A = 0 ; A < color_list.length ; A ++ ){
+  $(`#color_${color_list[A].colornum}`).css("background-color",`#${color_list[A].color_code}`);
+  $(`#colorname_${color_list[A].colornum}`).text(color_list[A].colorname);
+}
+
---
+++ footer.js
@@ -0,0 +1,32 @@
+// alert(1111);
+
+// 襟をおしたら襟のタブが出る
+
+// $("#footer_1_collar").on("click",function(){
+//   $(".footer_2").slideToggle();
+// });
+
+// $("#footer_1_collar").on("click",function(){
+//   $(".footer_3").slideToggle();
+// });
+
+// $("#footer_1_color").on("click",function(){
+//   $(".slide_footerA").slideToggle();
+// });
+
+
+// $("#footer_1_collar").on("click",function(){
+//   $(".slide_footerB").slideToggle();
+// });
+
+$("#footer_1_collar").on("click",function(){
+  $(".slide_footer_B").slideToggle();
+});
+
+$("#footer_1_color").on("click",function(){
+  $(".slide_footer_A").slideToggle();
+});
+
+
+
+
