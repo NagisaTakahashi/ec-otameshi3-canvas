92d19ae3fa92b24186cd1204563d9c08bb8d1e51
nagisa_takahashi@recouture-inc.com <nagisa_takahashi@outlook.jp>
2022-11-07 22:16:57 +0900

    まえたつさんコード読解、完コピ、再現まで完了

--- canvas.js
+++ canvas.js
@@ -29,23 +29,14 @@ const fillStartPosition = { //塗りが開始する場所
 
 // canvasタグ関連
 
-const can1 = $("#main_canvas1")[0];
+const can1 = document.getElementById("main_canvas1");
 const ctx1 = can1.getContext("2d");
 
-const can2 = $("#main_canvas2")[0];
+const can2 = document.getElementById("main_canvas2");
 const ctx2 = can2.getContext("2d");
 
 
-// カラーの定義
 
-var color_list= [
-  {"colornum":"01","colorname":"NVY","color_code":"3A3A49","RGB":"58,58,73",},
-  {"colornum":"02","colorname":"KKI","color_code":"5D6402","RGB":"93,100,2",},
-  {"colornum":"03","colorname":"LIME","color_code":"CFE375","RGB":"207,207,117",},
-  {"colornum":"04","colorname":"PPL","color_code":"C20F7C","RGB":"194,15,124",},
-  {"colornum":"05","colorname":"RED","color_code":"F32359","RGB":"243,35,89",},
-  {"colornum":"06","colorname":"MNT","color_code":"CBF1EF","RGB":"203,241,239",},
-];
 
 // for( let A = 0 ; A < color_list.length ; A ++ );
 
@@ -94,78 +85,60 @@ var color_list= [
 
 
 
-//なにがやぱいって、色を重ねるごとに、「切り替え」ではなく「上塗り」になることが判明！！canvaをいったんここで保存して、CSSでもブレンドモード（乗算）が使えそうなので、そっちで試してみる。
-
 
 //代入やめて、１回スでいれてみる（まえたつさん方式）
 
 
-  // var img = new Image();
-  // img.src = "img/clothed_3d/RC-N012.png";
+//1.洋服に対して色を乗算してカラーリング
 
-  // img.onload = function() {
-  // ctx1.drawImage(img, 0,0,346,570); //いったんcanvas内の画像はこのサイズに統一
-  // };
+ctx1.fillStyle = "rgba(0, 0, 0, 0)"; //最初は透明の背景
+ctx1.fillRect(0, 0, 346, 570);
 
-
-
-//洋服に対して色を乗算してカラーリング
-
-var imageObjA = new Image();
-imageObjA.src = "img/clothed_3d/RC-N012.png";
-imageObjA.onload = function () {
-  ctx1.drawImage(imageObjA, 0,0,346,570);
-}
-
-
-ctx1.fillStyle = "rgba(0, 0, 0, 0)";
-ctx1.fillRect(
-    fillStartPosition.x,
-    fillStartPosition.y,
-    canvasInfo.size.width,
-    canvasInfo.size.height
-);
 const imageDataPath = "img/clothed_3d/RC-N012.png";
 
-var imageObjA = new Image();
-imageObjA.onload = function () {
-    ctx1.drawImage(imageObjA, image.position.width, image.position.height, image.size.width, image.size.height);
-    // ctx1.drawImage(imageObjA, 0,0,346,570); //書き方どちらでも可
+var base_item = new Image();
+base_item.onload = function () {
+  ctx1.drawImage(base_item, 0,0,346,570);
 };
-imageObjA.src = imageDataPath;
+base_item.src = imageDataPath;
 ctx1.globalCompositeOperation = "multiply";
 
 
-// 洋服以外に対しての余白部分に対しての色の上書きによる視覚上の削除
+// 2.洋服以外に対しての余白部分に対しての色の上書きによる視覚上の削除
 ctx2.fillStyle = "white"; //外側を白色に埋めてる
-ctx2.fillRect(
-    fillStartPosition.x,
-    fillStartPosition.y,
-    canvasInfo.size.width,
-    canvasInfo.size.height
-);
-var imageObjB = new Image();
-imageObjB.onload = function () {
-    ctx2.drawImage(imageObjB, image.position.width, image.position.height, image.size.width, image.size.height);
-    // ctx2.drawImage(imageObjB, 0,0,346,570); //書き方どちらでも可
+ctx2.fillRect(0, 0, 346, 570);
+
+var mask = new Image();
+mask.onload = function () {
+    ctx2.drawImage(mask,0, 0, 346, 570);
 };
-imageObjB.src = imageDataPath;
+mask.src = imageDataPath;
 ctx2.globalCompositeOperation = "xor";//重なった部分を白く
 
 
-// // ボタンクリックした際に指定色を重ねる
+// 3.ボタンクリックした際に指定色を重ねる
+
+// 3-1.カラーの定義
+
+var color_list= [
+  {"colornum":"01","colorname":"NVY","color_code":"3A3A49","RGB":"58,58,73",},
+  {"colornum":"02","colorname":"KKI","color_code":"5D6402","RGB":"93,100,2",},
+  {"colornum":"03","colorname":"LIME","color_code":"CFE375","RGB":"207,207,117",},
+  {"colornum":"04","colorname":"PPL","color_code":"C20F7C","RGB":"194,15,124",},
+  {"colornum":"05","colorname":"RED","color_code":"F32359","RGB":"243,35,89",},
+  {"colornum":"06","colorname":"MNT","color_code":"CBF1EF","RGB":"203,241,239",},
+];
+
+// 3-2.カラーをオブジェクトから代入
 
 // const colorBtn = $(`#color_${color_list[A].colornum}`);
 
 
-
-
-
 // ボタンクリックした際に指定色を重ねる
 const colorButton = document.getElementById("btn");
+
 colorButton.addEventListener("click", function () {
     const color = document.getElementById("addColor");
-
     const rgbRed = parseInt(color.value.substring(1, 3), 16);
     const rgbGreen = parseInt(color.value.substring(3, 5), 16);
     const rgbBlue = parseInt(color.value.substring(5, 7), 16);
@@ -173,29 +146,22 @@ colorButton.addEventListener("click", function () {
 
     // clearRectしないと描画内容が新規更新されない。色がなぜか上塗りされる。１回クリアする
 
-    ctx1.clearRect(
-        fillStartPosition.x,
-        fillStartPosition.y,
-        canvasInfo.size.width,
-        canvasInfo.size.height
-    );
+    ctx1.clearRect(0, 0, 346, 570);
 
     //新たに選択したカラーで塗り
 
     ctx1.fillStyle = `rgba(${rgbRed},${rgbGreen},${rgbBlue}, 0.7)`;
-    ctx1.fillRect(
-        fillStartPosition.x,
-        fillStartPosition.y,
-        canvasInfo.size.width,
-        canvasInfo.size.height
-    );
+    ctx1.fillRect(0, 0, 346, 570);
 
-    var imageObjA = new Image();
-    imageObjA.onload = function () {
-        ctx1.drawImage(imageObjA, image.position.width, image.position.height);
+    var base_item = new Image();
+    base_item.onload = function () {
+        ctx1.drawImage(base_item,0,0,346,570); //おまえだったのかい！
     };
-    imageObjA.src = imageDataPath;
-});
+    base_item.src = imageDataPath;
+  }
+);
+
+
 
 
 
