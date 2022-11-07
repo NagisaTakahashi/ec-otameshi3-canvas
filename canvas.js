7bf3a221d0bb34f8138285d18a006b89e7fe3772
nagisa_takahashi@recouture-inc.com <nagisa_takahashi@outlook.jp>
2022-11-07 20:57:19 +0900

    いったん模写まで

--- canvas.js
+++ canvas.js
@@ -2,11 +2,38 @@
 
 // alert(1111);
 
+// キャンバスのサイズ管理
+const canvasInfo = {
+  size: { //キャンバスの大きさ
+      width: 346, 
+      height: 570,
+  },
+};
+const sizeUnit = "px";
+const image = {
+  position: { //画像が開始する場所
+      height: 0,
+      width: 0,
+  },
+  size: { //画像の大きさ
+    width: 346,
+    height: 570,
+  },
+};
+const fillStartPosition = { //塗りが開始する場所
+  x: 0,
+  y: 0,
+};
+
+
 
 // canvasタグ関連
 
-const can = $("#main_canvas")[0];
-const ctx = can.getContext("2d");
+const can1 = $("#main_canvas1")[0];
+const ctx1 = can1.getContext("2d");
+
+const can2 = $("#main_canvas2")[0];
+const ctx2 = can2.getContext("2d");
 
 
 // カラーの定義
@@ -20,40 +47,160 @@ var color_list= [
   {"colornum":"06","colorname":"MNT","color_code":"CBF1EF","RGB":"203,241,239",},
 ];
 
+// for( let A = 0 ; A < color_list.length ; A ++ );
 
 //上記のカラー定義から、カラーのサムネイルと、カラー名表記に代入して画面上で表示
-for( let A = 0 ; A < color_list.length ; A ++ ){
+// for( let A = 0 ; A < color_list.length ; A ++ ){
 
-  //このなかにいれてみる
+//   //   //このなかにいれてみる
 
-  // //この部分を画像にする
-  var img = new Image();
-  img.src = "img/clothed_3d/RC-N012.png";
+//   // //この部分を画像にする
+//   var img = new Image();
+//   img.src = "img/clothed_3d/RC-N012.png";
 
-  img.onload = function() {
-    ctx.drawImage(img, 0,0,346,570); //いったんcanvas内の画像はこのサイズに統一
+//   var img2 = new Image();
+//   img2.src = img.src;
 
-    //上に重ねるレイヤーの焼き込み方法指定
-    // ctx.globalCompositeOperation = "multiply";//乗算！！
-    // ctx.fillStyle = `rgba(${color_list[2].RGB},1)`;//色味と透過 乗算指定しているから透過させない
-    // ctx.fillRect( 0 , 0 , 346 , 570); //サイズと場所
+//   img.onload = function() {
 
-    ctx.globalCompositeOperation = "source-atop";//重なったところだけ！！マスキング！
-    // 上に重ねるレイヤーの色味と透過指定
-    ctx.fillStyle = `rgba(${color_list[2].RGB}, 0.5)`; //色味と透過 
-    ctx.fillRect( 0 , 0 , 346 , 570); //サイズと場所
+//     ctx1.drawImage(img, 0,0,346,570); //いったんcanvas内の画像はこのサイズに統一
+//     //上に重ねるレイヤーの焼き込み方法指定
+//     ctx1.fillStyle = `rgba(${color_list[2].RGB},0.5)`;//色味と透過 乗算指定しているから透過させない
+//     ctx1.fillRect( 0 , 0 , 346 , 570); //サイズと場所
+//     ctx1.globalCompositeOperation = "multiply";//乗算！！
 
-    $(`#color_${color_list[A].colornum}`).on("click",function(){
-      ctx.globalCompositeOperation = "source-atop";
-      ctx.fillStyle = `rgba(${color_list[A].RGB}, 0.5)`; //色味と透過 
-      ctx.fillRect( 0 , 0 , 346 , 570); //サイズと場所  
-    });
 
-  }
-}
+//     // // 上に重ねるレイヤーの色味と透過指定
+//     ctx2.drawImage(img, 0,0,346,570); //いったんcanvas内の画像はこのサイズに統一
+//     ctx2.fillStyle = "white"; //色味と透過 白で１
+//     ctx2.fillRect( 0 , 0 , 346 , 570); //サイズと場所
+//     ctx2.globalCompositeOperation = "xor";//重なったところだけ！！マスキング！
+
+    
+//     // // 上に重ねるレイヤーの色味と透過指定
+//     // ctx2.fillStyle = `rgba(255,255,255,1)`; //色味と透過 白で１
+//     // ctx2.fillRect( 0 , 0 , 346 , 570); //サイズと場所
+//     // ctx2.globalCompositeOperation = "xor";//重なったところだけ！！マスキング！
+
+//     // $(`#color_${color_list[A].colornum}`).on("click",function(){
+//     //   ctx.globalCompositeOperation = "source-atop";
+//     //   ctx.fillStyle = `rgba(${color_list[A].RGB}, 0.5)`; //色味と透過 
+//     //   ctx.fillRect( 0 , 0 , 346 , 570); //サイズと場所  
+//     // });
+
+//   }
+// }
+
+
 
 
 //なにがやぱいって、色を重ねるごとに、「切り替え」ではなく「上塗り」になることが判明！！canvaをいったんここで保存して、CSSでもブレンドモード（乗算）が使えそうなので、そっちで試してみる。
+
+
+//代入やめて、１回スでいれてみる（まえたつさん方式）
+
+
+  // var img = new Image();
+  // img.src = "img/clothed_3d/RC-N012.png";
+
+  // img.onload = function() {
+  // ctx1.drawImage(img, 0,0,346,570); //いったんcanvas内の画像はこのサイズに統一
+  // };
+
+
+
+//洋服に対して色を乗算してカラーリング
+
+var imageObjA = new Image();
+imageObjA.src = "img/clothed_3d/RC-N012.png";
+imageObjA.onload = function () {
+  ctx1.drawImage(imageObjA, 0,0,346,570);
+}
+
+
+ctx1.fillStyle = "rgba(0, 0, 0, 0)";
+ctx1.fillRect(
+    fillStartPosition.x,
+    fillStartPosition.y,
+    canvasInfo.size.width,
+    canvasInfo.size.height
+);
+const imageDataPath = "img/clothed_3d/RC-N012.png";
+
+var imageObjA = new Image();
+imageObjA.onload = function () {
+    ctx1.drawImage(imageObjA, image.position.width, image.position.height, image.size.width, image.size.height);
+    // ctx1.drawImage(imageObjA, 0,0,346,570); //書き方どちらでも可
+};
+imageObjA.src = imageDataPath;
+ctx1.globalCompositeOperation = "multiply";
+
+
+// 洋服以外に対しての余白部分に対しての色の上書きによる視覚上の削除
+ctx2.fillStyle = "white"; //外側を白色に埋めてる
+ctx2.fillRect(
+    fillStartPosition.x,
+    fillStartPosition.y,
+    canvasInfo.size.width,
+    canvasInfo.size.height
+);
+var imageObjB = new Image();
+imageObjB.onload = function () {
+    ctx2.drawImage(imageObjB, image.position.width, image.position.height, image.size.width, image.size.height);
+    // ctx2.drawImage(imageObjB, 0,0,346,570); //書き方どちらでも可
+};
+imageObjB.src = imageDataPath;
+ctx2.globalCompositeOperation = "xor";//重なった部分を白く
+
+
+// // ボタンクリックした際に指定色を重ねる
+
+// const colorBtn = $(`#color_${color_list[A].colornum}`);
+
+
+
+
+
+// ボタンクリックした際に指定色を重ねる
+const colorButton = document.getElementById("btn");
+colorButton.addEventListener("click", function () {
+    const color = document.getElementById("addColor");
+
+    const rgbRed = parseInt(color.value.substring(1, 3), 16);
+    const rgbGreen = parseInt(color.value.substring(3, 5), 16);
+    const rgbBlue = parseInt(color.value.substring(5, 7), 16);
+    console.log(`RGB: ${rgbRed},${rgbGreen},${rgbBlue}`);
+
+    // clearRectしないと描画内容が新規更新されない。色がなぜか上塗りされる。１回クリアする
+
+    ctx1.clearRect(
+        fillStartPosition.x,
+        fillStartPosition.y,
+        canvasInfo.size.width,
+        canvasInfo.size.height
+    );
+
+    //新たに選択したカラーで塗り
+
+    ctx1.fillStyle = `rgba(${rgbRed},${rgbGreen},${rgbBlue}, 0.7)`;
+    ctx1.fillRect(
+        fillStartPosition.x,
+        fillStartPosition.y,
+        canvasInfo.size.width,
+        canvasInfo.size.height
+    );
+
+    var imageObjA = new Image();
+    imageObjA.onload = function () {
+        ctx1.drawImage(imageObjA, image.position.width, image.position.height);
+    };
+    imageObjA.src = imageDataPath;
+});
+
+
+
+
+
 
 
 
